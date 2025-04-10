ff3bfa4123b2f321c7108e951603f67c447b4b4a
test2-at-462965162602 <69719073+zaid714-shaikh@users.noreply.github.com>
2025-04-10 19:30:23 +0530

    project added

---
+++ SB-Github/src/main/java/com/SbGithubApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbGithubApplication.class, args);
+	}
+
+}

5751c6596f0b521013444df68801e7e48acc2db1
test2-at-462965162602 <69719073+zaid714-shaikh@users.noreply.github.com>
2025-04-10 19:33:45 +0530

    Homecontroller added with m1 method

---
+++ SB-Github/src/main/java/com/controller/Homecontroller.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Homecontroller {
+
+	@PostMapping("/")
+	public void m1() {
+		System.out.println("Hi");
+	}
+}
